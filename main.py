19340616569c0fa2ec9d5902d7ba0783b69f7931
Michael_Kim <michael.kim1002@gmail.com>
2025-08-08 00:42:34 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template , redirect, url_for , send_from_directory
+from flask_bootstrap import Bootstrap5
+import csv
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    with open('michaelkim-portfolio.csv', newline='', encoding='utf-8') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',')
+        list_of_rows = []
+        for row in csv_data:
+            list_of_rows.append(row)
+        print(list_of_rows)
+    return render_template("index.html", projects=list_of_rows)
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+@app.route('/download')
+def download():
+    return send_from_directory(
+        "static", path = "files/MichaelKimResume.pdf"
+    )
+if __name__ == "__main__":
+    app.run(debug=True)
