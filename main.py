a23b31f71a02cbcb9ffcb8f209c9a2d407bcd6f8
Michael_Kim <michael.kim1002@gmail.com>
2025-09-21 18:51:54 -0500

    Update code for AWS

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template , redirect, url_for , send_from_directo
 from flask_bootstrap import Bootstrap5
 import os
 import csv
-app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-Bootstrap5(app)
-@app.route("/")
+application = Flask(__name__)
+application.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+Bootstrap5(application)
+@application.route("/")
 def home():
     with open('michaelkim-portfolio.csv', newline='', encoding='utf-8') as csv_file:
         csv_data = csv.reader(csv_file, delimiter=',')
@@ -14,13 +14,13 @@ def home():
             list_of_rows.append(row)
         print(list_of_rows)
     return render_template("index.html", projects=list_of_rows)
-@app.route("/resume")
+@application.route("/resume")
 def resume():
     return render_template("resume.html")
-@app.route('/download')
+@application.route('/download')
 def download():
     return send_from_directory(
         "static", path = "files/MichaelKimResume.pdf"
     )
 if __name__ == "__main__":
-    app.run(debug=False, port=5003)
+    application.run(debug=False, port=5003)
